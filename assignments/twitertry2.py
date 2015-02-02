e6123fb883077102b4cf419a40f5672d28d31575
Anuj Deshpande <anujdeshpande92@gmail.com>
2015-02-02 12:42:33 +0530

    Move schematics and design files to hardware/
    Remove old assignments

--- assignments/twitertry2.py
+++
@@ -1,6 +0,0 @@
-import twitter
-api = twitter.Api()
-
-input=raw_input('Enter user name ')
-statuses = api.GetUserTimeline(input)
-print [s.text for s in statuses]
--- assignments/twittertry3.py
+++
@@ -1,5 +0,0 @@
-import twitter
-api = twitter.Api()
-
-statuses=api.GetPublicTimeline()
-print [s.user.name for s in statuses]
